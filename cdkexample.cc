3287105dbea6bd95d52a313ad965a78716632c1b
carlosesponda <cfe160030@utdallas.edu>
2018-04-23 03:17:32 -0500

    record files now display

--- cdkexample.cc
+++ cdkexample.cc
@@ -30,6 +30,15 @@ public:
   uint64_t numRecords;
 };
 
+const int maxRecordStringLength = 25;
+
+class BinaryFileRecord
+{
+public:
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+};
+
 int main()
 {
 
@@ -79,8 +88,16 @@ int main()
 
   //reads the header binary file
   BinaryFileHeader *myHeader = new BinaryFileHeader();
+  BinaryFileRecord *myRecord = new BinaryFileRecord();
+  BinaryFileRecord *myRecord2 = new BinaryFileRecord();
+  BinaryFileRecord *myRecord3 = new BinaryFileRecord();
+  BinaryFileRecord *myRecord4 = new BinaryFileRecord();
   ifstream binInfile ("cs3377.bin", ios::in | ios::binary);
   binInfile.read( (char *)myHeader , sizeof(BinaryFileHeader));
+  binInfile.read( (char *)myRecord , sizeof(BinaryFileRecord));
+  binInfile.read( (char *)myRecord2 , sizeof(BinaryFileRecord));
+  binInfile.read( (char *)myRecord3 , sizeof(BinaryFileRecord));
+  binInfile.read( (char *)myRecord4 , sizeof(BinaryFileRecord));
   binInfile.close();
 
   /*
@@ -89,15 +106,35 @@ int main()
   char Magic  [50];
   char Version [50];
   char NumRecords [50];
+  char strlen1 [50];
+  char strlen2 [50];
+  char strlen3 [50];
+  char strlen4 [50];
   sprintf(Magic,"%#X", myHeader->magicNumber);
   sprintf(Version,"%u",myHeader->versionNumber);
   sprintf(NumRecords, "%u", myHeader->numRecords);
+  sprintf(strlen1, "%u", myRecord->strLength);
+  sprintf(strlen2, "%u", myRecord2->strLength);
+  sprintf(strlen3, "%u", myRecord3->strLength);
+  sprintf(strlen4, "%u", myRecord4->strLength);
   std::string box1 = std::string("Magic: ") + Magic;
   std::string box2 = std::string("Version: ") + Version;
   std::string box3 = std::string("NumRecords: ") + NumRecords;
+  std::string box4 = std::string("strlen: ") + strlen1;
+  std::string box7 = std::string("strlen: ") + strlen2;
+  std::string box10 = std::string("strlen: ") + strlen3;
+  std::string box13 = std::string("strlen: ") + strlen4;
   setCDKMatrixCell(myMatrix, 1, 1, box1.c_str());
   setCDKMatrixCell(myMatrix, 1, 2, box2.c_str());
   setCDKMatrixCell(myMatrix, 1, 3, box3.c_str());
+  setCDKMatrixCell(myMatrix, 2, 1, box4.c_str());
+  setCDKMatrixCell(myMatrix, 2, 2, myRecord->stringBuffer);
+  setCDKMatrixCell(myMatrix, 3, 1, box7.c_str());
+  setCDKMatrixCell(myMatrix, 3, 2, myRecord2->stringBuffer);
+  setCDKMatrixCell(myMatrix, 4, 1, box10.c_str());
+  setCDKMatrixCell(myMatrix, 4, 2, myRecord3->stringBuffer);
+  setCDKMatrixCell(myMatrix, 5, 1, box13.c_str());
+  setCDKMatrixCell(myMatrix, 5, 2, myRecord4->stringBuffer);
   drawCDKMatrix(myMatrix, true);    /* required  */
 
   /* So we can see results, pause until a key is pressed. */
